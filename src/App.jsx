97a7905939fc2c518c5070863fd048cb64e5e97c
eromose-le <erons24@yahoo.com>
2022-07-12 15:46:35 +0100

    feat: setup firebase and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        {/* <Route path="/user/:id" element={<Dashboard />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
